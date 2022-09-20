64e4d63de89cf83db58b12a6e2e023dd1a3187eb
xErrorAmelie <51492094+xErrorAmelie@users.noreply.github.com>
2022-09-20 02:53:11 +0200

    yes silly lynn, this is called "update"

--- app/src/main/java/com/example/myapplication/ui/einstellungen/EinstellungenFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/einstellungen/EinstellungenFragment.kt
@@ -1,15 +1,18 @@
 package com.example.myapplication.ui.einstellungen
 
+import android.R
 import android.content.Context
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.method.PasswordTransformationMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.EditText
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.databinding.FragmentEinstellungenBinding
+
 
 class EinstellungenFragment : Fragment() {
 
